9e1a6cb1b56e37e6f50e3645ecc695d9a5863ca4
lapan13 <ariirwandi13@gmail.com>
2020-09-02 17:47:45 +0700

    create module & component for homepage

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import "/src/component/menu-bar.js";
-import "/src/component/top-bar.js";
-import "/src/component/main-content.js";
-import "/src/component/side-content.js";
+import "/src/component/home-page.js";
 import main from '/src/script/main.js';
 
 document.addEventListener("DOMContentLoaded", main);
